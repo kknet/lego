3999ff7b2f8a0806c50572e07d495a8c9e800e17
actantion <672817221@qq.com>
2019-08-19 16:13:58 -0400

    add kcp

---
+++ third_party/kcp/CMakeFiles/kcp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kcp.dir/ikcp.c.o"
+  "libkcp.a"
+  "libkcp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/kcp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
