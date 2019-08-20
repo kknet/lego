43ad4e9df72368615ef7c81d78864f24c32f3936
actantion <672817221@qq.com>
2019-08-20 14:55:53 +0800

    fix common utils

--- src/common/utils.cc
+++ src/common/utils.cc
@@ -92,6 +92,43 @@ uint32_t RandomCountry() {
     return rand() % FX;
 }
 
+void itimeofday(long *sec, long *usec) {
+#if defined(__unix)
+	struct timeval time;
+	gettimeofday(&time, NULL);
+	if (sec) *sec = time.tv_sec;
+	if (usec) *usec = time.tv_usec;
+#else
+	static long mode = 0, addsec = 0;
+	BOOL retval;
+	static int64_t freq = 1;
+	int64_t qpc;
+	if (mode == 0) {
+		retval = QueryPerformanceFrequency((LARGE_INTEGER*)&freq);
+		freq = (freq == 0) ? 1 : freq;
+		retval = QueryPerformanceCounter((LARGE_INTEGER*)&qpc);
+		addsec = (long)time(NULL);
+		addsec = addsec - (long)((qpc / freq) & 0x7fffffff);
+		mode = 1;
+	}
+	retval = QueryPerformanceCounter((LARGE_INTEGER*)&qpc);
+	retval = retval * 2;
+	if (sec) *sec = (long)(qpc / freq) + addsec;
+	if (usec) *usec = (long)((qpc % freq) * 1000000 / freq);
+#endif
+}
+
+int64_t iclock64(void) {
+	long s, u;
+	int64_t value;
+	itimeofday(&s, &u);
+	value = ((int64_t)s) * 1000 + (u / 1000);
+	return value;
+}
+
+uint32_t iclock() {
+	return static_cast<uint32_t>(iclock64() & 0xfffffffful);
+}
 }  // namespace common
 
 }  // namespace lego
