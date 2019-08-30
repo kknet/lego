32ddac43240790edf3b600165dd8d8d5b625be08
actantion <672817221@qq.com>
2019-08-30 11:23:51 +0800

    fix bug

--- src/bft/member_manager.cc
+++ src/bft/member_manager.cc
@@ -63,13 +63,6 @@ bool MemberManager::IsLeader(
     if (mem_ptr->id == node_id) {
         return true;
     }
-
-    BFT_ERROR("is not leader: %s, %s",
-            common::Encode::HexEncode(node_id).c_str(),
-            common::Encode::HexEncode(mem_ptr->id).c_str());
-    for (auto iter = member_ptr->begin(); iter != member_ptr->end(); ++iter) {
-        BFT_ERROR("%s", common::Encode::HexEncode((*iter)->id).c_str());
-    }
     return false;
 }
 
