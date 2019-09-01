b8c2bbcdafa4ba3be4422a005439041a968e0896
actantion <672817221@qq.com>
2019-09-01 16:06:18 +0800

    add client transaction

--- src/client/proto/client_proto.h
+++ src/client/proto/client_proto.h
@@ -5,6 +5,7 @@
 #include "transport/proto/transport.pb.h"
 #include "transport/transport_utils.h"
 #include "dht/node.h"
+#include "dht/dht_key.h"
 #include "client/proto/client.pb.h"
 #include "client/client_utils.h"
 
