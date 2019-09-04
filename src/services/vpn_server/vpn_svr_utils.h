7657e4e2f44428126bab8e7ca166553e311cff83
actantion <672817221@qq.com>
2019-09-04 20:47:06 +0800

    support vpn server

--- src/services/vpn_server/vpn_svr_utils.h
+++ src/services/vpn_server/vpn_svr_utils.h
@@ -17,20 +17,6 @@ enum VpnSvrErrorCode {
     kVpnsvrError = 1,
 };
 
-static const std::vector<std::pair<std::string, uint16_t>> kEncryptTypeVec = {
-    std::make_pair<std::string, uint16_t>("bf-cfb", 7453),
-    std::make_pair<std::string, uint16_t>("aes-128-cfb",10455),
-    std::make_pair<std::string, uint16_t>("aes-192-cfb",6456),
-    std::make_pair<std::string, uint16_t>("aes-256-cfb",5457),
-    std::make_pair<std::string, uint16_t>("camellia-128-cfb", 4458),
-    std::make_pair<std::string, uint16_t>("camellia-192-cfb", 3459),
-    std::make_pair<std::string, uint16_t>("camellia-256-cfb", 2460),
-    std::make_pair<std::string, uint16_t>("chacha20", 7961),
-    std::make_pair<std::string, uint16_t>("chacha20-ietf", 7762),
-    std::make_pair<std::string, uint16_t>("rc4-md5", 7963)
-};
-static const std::string kMode = "tcp_and_udp";
-
 }  // namespace vpn
 
 }  // namespace lego
