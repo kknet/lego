0afdaad4be52bba7504fd172d4425bf8bba80cff
actantion <672817221@qq.com>
2019-09-20 17:30:53 +0800

    fix vpn server crypto

--- src/services/vpn_server/vpn_svr_utils.h
+++ src/services/vpn_server/vpn_svr_utils.h
@@ -6,7 +6,7 @@
 #define VPNSVR_DEBUG(fmt, ...) DEBUG("[vpn_svr]" fmt, ## __VA_ARGS__)
 #define VPNSVR_INFO(fmt, ...) INFO("[vpn_svr]" fmt, ## __VA_ARGS__)
 #define VPNSVR_WARN(fmt, ...) WARN("[vpn_svr]" fmt, ## __VA_ARGS__)
-#define VPNSVR_ERROR(fmt, ...) ERROR("[vpn_svr]" fmt, ## __VA_ARGS__)
+#define VPNSVR_ERROR(fmt, ...) WARN("[vpn_svr]" fmt, ## __VA_ARGS__)
 
 namespace lego {
 
