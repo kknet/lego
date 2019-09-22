47c62901c97f9619be5a39f4bf3b4cd1c99723e7
actantion <672817221@qq.com>
2019-09-22 16:49:27 +0800

    fix default listen ctx

--- src/services/vpn_server/vpn_server.cc
+++ src/services/vpn_server/vpn_server.cc
@@ -1634,7 +1634,7 @@ int VpnServer::Init(
     }
     default_ctx_->vpn_port = kDefaultVpnPort;
     cork_dllist_init(&default_ctx_->svr_item->connections);
-    listen_ctx->thread_ptr = std::make_shared<std::thread>(&StartVpn, default_ctx_.get());
+    default_ctx_->thread_ptr = std::make_shared<std::thread>(&StartVpn, default_ctx_.get());
     // InitSignal(default_ctx_);
 
     staking_tick_.CutOff(
