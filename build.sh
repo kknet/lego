df0d359bd43f3f71a4e06aed816d8457c7cf87be
actantion <672817221@qq.com>
2019-10-08 14:54:52 +0800

    fix linux build bug

--- build.sh
+++ build.sh
@@ -7,13 +7,12 @@ cd cbuild
 #   Release:            -O3 -DNDEBUG
 #   RelWithDebInfo:     -O2 -g -DNDEBUG
 #   MinSizeRel:         -Os -DNDEBUG
-cmake .. -DCMAKE_BUILD_TYPE=Debug -DOPENSSL_ROOT_DIR=/usr/local/Cellar/openssl/1.0.2r -DBUILD_SHARED_LIBS=OFF -DCMAKE_INSTALL_PREFIX=~/lego
+cmake .. -DCMAKE_BUILD_TYPE=Debug -DOPENSSL_ROOT_DIR=/usr/local/Cellar/openssl/1.0.2r -DCMAKE_INSTALL_PREFIX=~/lego
 
 # make
-#make lego -j4 
+make lego -j4 
 make vpn_proxy -j4 
-#make vpn_client -j4 
-#make top_node
+make vpn_client -j4 
 
 OS=`uname`
 if [ "$OS" == "Darwin" ]
