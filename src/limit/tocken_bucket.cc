59190a108c059d3e1a4e0eb02f62f5c2f5de26eb
actantion <672817221@qq.com>
2019-10-26 18:04:59 +0800

    fix bug

--- src/limit/tocken_bucket.cc
+++ src/limit/tocken_bucket.cc
@@ -7,43 +7,59 @@ namespace lego {
 namespace limit {
 
 bool TockenBucket::UpCheckLimit(uint32_t stream) {
-    auto period_ms = common::TimeUtils::PeriodMs(pre_timestamp_);
+    auto period_ms = common::TimeUtils::PeriodMs(pre_up_timestamp_);
     if (period_ms >= kIncreaseTockenPeriod) {
         up_tockens_ += static_cast<int32_t>(period_ms * tockens_add_per_ms_);
         if (up_tockens_ > max_tockens_) {
             up_tockens_ = max_tockens_;
         }
-        pre_timestamp_ = std::chrono::system_clock::now();
+        pre_up_timestamp_ = std::chrono::system_clock::now();
     }
 
-    if (down_tockens_ <= 0) {
-        return false;
-    }
-
-    if (up_tockens_ < stream) {
-        up_tockens_ = 0;
-        return false;
-    }
-
-    up_tockens_ -= stream;
-    return true;
-}
-
-bool TockenBucket::DownCheckLimit(uint32_t stream) {
-    auto period_ms = common::TimeUtils::PeriodMs(pre_timestamp_);
+    auto period_ms = common::TimeUtils::PeriodMs(pre_down_timestamp_);
     if (period_ms >= kIncreaseTockenPeriod) {
         down_tockens_ += static_cast<uint32_t>(period_ms * tockens_add_per_ms_);
         if (down_tockens_ > max_tockens_) {
             down_tockens_ = max_tockens_;
         }
+        pre_down_timestamp_ = std::chrono::system_clock::now();
+        if (down_tockens_ > 1024) {
+            download_valid_ = true;
+        }
+    }
+
+    if (!download_valid_) {
+        return false;
+    }
+
+    if (up_tockens_ < stream) {
+        return false;
+    }
+
+    up_tockens_ -= stream;
+    std::cout << "up tokens: " << up_tockens_ << std::endl;
+    return true;
+}
+
+bool TockenBucket::DownCheckLimit(uint32_t stream) {
+    auto period_ms = common::TimeUtils::PeriodMs(pre_down_timestamp_);
+    if (period_ms >= kIncreaseTockenPeriod) {
+        down_tockens_ += static_cast<uint32_t>(period_ms * tockens_add_per_ms_);
+        if (down_tockens_ > max_tockens_) {
+            down_tockens_ = max_tockens_;
+        }
+        pre_down_timestamp_ = std::chrono::system_clock::now();
     }
 
     if (down_tockens_ < stream) {
-        down_tockens_ = 0;
+        download_valid_ = false;
         return false;
     }
 
     down_tockens_ -= stream;
+    download_valid_ = true;
+    std::cout << "down tokens: " << up_tockens_ << std::endl;
+
     return true;
 }
 
--- src/limit/tocken_bucket.h
+++ src/limit/tocken_bucket.h
@@ -11,9 +11,12 @@ namespace limit {
 class TockenBucket : public LimitInterface {
 public:
     TockenBucket(int32_t max_tockens)
-            : max_tockens_(max_tockens),
+            : up_tockens_(max_tockens),
+              down_tockens_(max_tockens),
+              max_tockens_(max_tockens),
               tockens_add_per_ms_(static_cast<float>(max_tockens_) / 1000.0f) {
-        pre_timestamp_ = std::chrono::system_clock::now();
+        pre_up_timestamp_ = std::chrono::system_clock::now();
+        pre_down_timestamp_ = std::chrono::system_clock::now();
     }
     virtual ~TockenBucket() {}
     virtual bool UpCheckLimit(uint32_t stream);
@@ -22,11 +25,13 @@ public:
 private:
     static const uint32_t kIncreaseTockenPeriod = 10u;
 
-    std::chrono::system_clock::time_point pre_timestamp_;
+    std::chrono::system_clock::time_point pre_up_timestamp_;
+    std::chrono::system_clock::time_point pre_down_timestamp_;
     int32_t up_tockens_;
     int32_t down_tockens_;
     int32_t max_tockens_;
     float tockens_add_per_ms_;
+    bool download_valid_{ true };
 
     DISALLOW_COPY_AND_ASSIGN(TockenBucket);
 };
