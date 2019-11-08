faf3e173d2973a3dac6d81b1fc514244be4be009
actantion <672817221@qq.com>
2019-11-08 12:08:51 +0800

    add bandwidth debug for client

--- src/services/vpn_server/vpn_server.h
+++ src/services/vpn_server/vpn_server.h
@@ -60,7 +60,7 @@ private:
     static const uint32_t kAccountCheckPeriod = 10 * 1000 * 1000;
     static const uint32_t kConnectInitBandwidth = 10 * 1024 * 1024;
     static const uint32_t kAddBandwidth = 50 * 1024 * 1024;
-    static const uint32_t kMaxBandwidthFreeUse = 200 * 1024 * 1024;
+    static const uint32_t kMaxBandwidthFreeUse = 128 * 1024 * 1024;
 
     common::ThreadSafeQueue<StakingItemPtr> staking_queue_;
     common::ThreadSafeQueue<BandwidthInfoPtr> bandwidth_queue_;
