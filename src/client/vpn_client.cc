a31308bcc6c01d2421a15567c1d716cbfde3f5d8
actantion <672817221@qq.com>
2019-12-20 19:17:25 +0800

    fix client seeds

--- src/client/vpn_client.cc
+++ src/client/vpn_client.cc
@@ -825,24 +825,24 @@ std::string VpnClient::GetVpnServerNodes(
             return "OK";
         }
     } else {
-        if (vpn_vip_level_ != common::kNotVip) {
-            std::lock_guard<std::mutex> guard(vip_route_nodes_map_mutex_);
-            auto iter = vip_route_nodes_map_.find(country);
-            if (iter == vip_route_nodes_map_.end()) {
-                vip_route_nodes_map_[country] = std::deque<VpnServerNodePtr>();
-            } else {
-                for (auto qiter = iter->second.begin(); qiter != iter->second.end(); ++qiter) {
-                    (*qiter)->route_port = common::GetVpnRoutePort(
-                        common::Encode::HexDecode((*qiter)->dht_key),
-                        common::TimeUtils::TimestampDays());
-                    nodes.push_back(*qiter);
-                }
-
-                if (!nodes.empty()) {
-                    return "OK";
-                }
-            }
-        }
+//         if (vpn_vip_level_ != common::kNotVip) {
+//             std::lock_guard<std::mutex> guard(vip_route_nodes_map_mutex_);
+//             auto iter = vip_route_nodes_map_.find(country);
+//             if (iter == vip_route_nodes_map_.end()) {
+//                 vip_route_nodes_map_[country] = std::deque<VpnServerNodePtr>();
+//             } else {
+//                 for (auto qiter = iter->second.begin(); qiter != iter->second.end(); ++qiter) {
+//                     (*qiter)->route_port = common::GetVpnRoutePort(
+//                         common::Encode::HexDecode((*qiter)->dht_key),
+//                         common::TimeUtils::TimestampDays());
+//                     nodes.push_back(*qiter);
+//                 }
+// 
+//                 if (!nodes.empty()) {
+//                     return "OK";
+//                 }
+//             }
+//         }
 
         {
             std::lock_guard<std::mutex> guard(route_nodes_map_mutex_);
